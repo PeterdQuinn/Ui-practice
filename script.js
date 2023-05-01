23e1069f8c623e0916dedc6da3b8c6419e1221c5
Peter Quinn <peterdquinnsr@gmail.com>
2023-05-01 16:21:18 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add a click event listener to the CTA button
+document.querySelector('.cta-button').addEventListener('click', function() {
+    // Scroll to the bottom of the page
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+  });
+  
